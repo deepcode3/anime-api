98d28ee18b23a5fe9c00350270731f31357873f7
deepikanaik <deepika.naik@robosoftin.com>
2022-11-21 15:14:19 +0530

    api called in detail page

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,7 @@ function Home(){
     <div className={styles.container}>
         
         <Link  href='/signUp'><button>signup</button></Link>
-    
-        <Link  href='/anime'><button>anime list</button></Link>
-        <Link  href='/animeSSR'><button>anime list using SSR</button></Link>
+
         <Link  href='/animestatic'><button>anime list using static</button> </Link>
     </div>
   )
